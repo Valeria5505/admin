416ccdf368c17240e56be1fe13780a88282618f2
Valeria5505 <valeria.gerusenko@yandex.ru>
2017-06-08 17:19:19 +0700

    ош5

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?
 
-require_once ('/core/Application.php');
-require_once ('/core/Controller.php');
-require_once ('/core/DB.php');
-require_once ('/core/interface/iTemplate.php');
-require_once ('/core/FTemplate.php');
-require_once ('/core/Model.php');
-require_once ('/core/Request.php');
-require_once ('/application/models/User.php');
-require_once ('/application/models/Project.php');
-require_once ('/application/models/Curator.php');
-require_once ('/application/models/Constants.php');
-require_once ('/application/models/Release.php');
+require_once ('core/Application.php');
+require_once ('core/Controller.php');
+require_once ('core/DB.php');
+require_once ('core/interface/iTemplate.php');
+require_once ('core/FTemplate.php');
+require_once ('core/Model.php');
+require_once ('core/Request.php');
+require_once ('application/models/User.php');
+require_once ('application/models/Project.php');
+require_once ('application/models/Curator.php');
+require_once ('application/models/Constants.php');
+require_once ('application/models/Release.php');
 
 try{
 	
