b5c170b0401dd6be67de6d9b92a401a7f5de18ba
Valeria5505 <valeria.gerusenko@yandex.ru>
2017-06-08 19:43:10 +0700

    бд подключение

--- core/DB.php
+++ core/DB.php
@@ -19,10 +19,10 @@ class DB
 
 	private  function __construct()
 	{
-        $dbconfig = require('db_params.php');
-        //$this->pdo = new \PDO('mysql:host=$dbconfig["host"];dbname=$dbconfig["db"];charset=UTF8;', '$dbconfig["root"]', '$dbconfig["password"]');
+        $dbconfig = require('../../db_params.php');
+        $this->pdo = new \PDO('mysql:host=$dbconfig["host"];dbname=$dbconfig["db"];charset=UTF8;', '$dbconfig["root"]', '$dbconfig["password"]');
         //$this->pdo = new \PDO('mysql:host=localhost;dbname=host-test;charset=UTF8;', 'root', '');
-        $this->pdo = new \PDO('mysql:host=localhost;dbname=web_38;charset=UTF8;', 'web_38', 'sIJj758vvx6SgZXB');
+        //$this->pdo = new \PDO('mysql:host=localhost;dbname=web_38;charset=UTF8;', 'web_38', 'sIJj758vvx6SgZXB');
 	}
 
 	private function __clone()
